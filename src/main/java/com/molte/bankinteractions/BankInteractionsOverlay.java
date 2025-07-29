ae3fc02d8247d16b8aa8154a3e70f1a8b1c51f12
Moltenrain <139096533+Moltenrain@users.noreply.github.com>
2025-07-29 21:39:59 +1000

    Started adding in overlay system

---
+++ src/main/java/com/molte/bankinteractions/BankInteractionsOverlay.java
@@ -0,0 +1,54 @@
+package com.molte.bankinteractions;
+
+import net.runelite.api.Client;
+import net.runelite.api.Point;
+import net.runelite.client.ui.overlay.*;
+import net.runelite.client.ui.overlay.components.LineComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+
+public class BankInteractionsOverlay extends OverlayPanel
+{
+    private static final Point TOOLTIP_OFFSET = new Point(-40,  5);
+    private static final Font FONT = new Font("Arial", Font.PLAIN, 9);
+
+    private final Client _client;
+    private String _renderText = null;
+
+
+    @Inject
+    BankInteractionsOverlay(Client client){
+        _client = client;
+
+        setPriority(OverlayPriority.HIGHEST);
+        setPosition(OverlayPosition.DYNAMIC);
+        setLayer(OverlayLayer.ALWAYS_ON_TOP);
+    }
+
+
+    @Override
+    public Dimension render(Graphics2D graphics2D) {
+        final Point mousePosition = _client.getMouseCanvasPosition();
+
+        if (_renderText == null){
+            return super.render(graphics2D);
+        }
+
+        panelComponent.setPreferredLocation(new java.awt.Point(mousePosition.getX() + TOOLTIP_OFFSET.getX(), mousePosition.getY() + TOOLTIP_OFFSET.getY()));
+        panelComponent.setPreferredSize(new Dimension(25, 15));
+
+
+        panelComponent.getChildren().add(LineComponent.builder()
+                .left(_renderText)
+                .leftFont(FONT)
+                .build());
+
+        return super.render(graphics2D);
+    }
+
+    public void setRenderText(String text){
+        _renderText = text;
+    }
+}
